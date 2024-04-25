07eb5504ecc37363e90a4a61b4a7f6245dec64ff
seba_ <seba_@Compu>
2024-04-25 17:02:41 -0300

    Avance ejercicio 1

--- tp02_CocaMikaela/src/ar/edu/unju/fi/ejercicio01/main/Main.java
+++ tp02_CocaMikaela/src/ar/edu/unju/fi/ejercicio01/main/Main.java
@@ -1,9 +1,46 @@
 package ar.edu.unju.fi.ejercicio01.main;
 
+import java.util.*;
+import ar.edu.unju.fi.ejercicio01.model.Producto;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		List<Producto> lista = new ArrayList<>();
+		
+		int op;
+		do{
+		System.out.println("Seleccione una opcion");
+		System.out.println("1-Crear producto");
+		System.out.println("2-Mostrar productos");
+		System.out.println("3-Modificar producto");
+		System.out.println("4-Salir");
+		op = sc.nextInt();
+		switch(op)
+		{
+		case 1: 
+			
+		break;
+		
+		case 2:
+			
+		break;
+		
+		case 3:
+			
+		break;
+		
+		case 4:
+			System.out.println("Finalizando programa");
+		break;
+		
+		default:
+			System.out.println("Opcion invalida");
+		}
+		}	
+		while(op!=4);
 
 	}
 
--- tp02_CocaMikaela/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ tp02_CocaMikaela/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -1,5 +1,80 @@
 package ar.edu.unju.fi.ejercicio01.model;
 
 public class Producto {
+	
+	private String codigo;
+	private String descripcion;
+	private double precio_unitario;
+	private Origen_Fabricacion origen;
+	private Categoria categoria;
+	
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Producto(String codigo, String descripcion, double precio_unitario, Origen_Fabricacion origen,
+			Categoria categoria) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precio_unitario = precio_unitario;
+		this.origen = origen;
+		this.categoria = categoria;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
 
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio_unitario() {
+		return precio_unitario;
+	}
+
+	public void setPrecio_unitario(double precio_unitario) {
+		this.precio_unitario = precio_unitario;
+	}
+
+	public Origen_Fabricacion getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(Origen_Fabricacion origen) {
+		this.origen = origen;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+
+	public enum Origen_Fabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY; 
+	}
+	
+	public enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", descripcion=" + descripcion + ", precio_unitario=" + precio_unitario
+				+ ", origen=" + origen + ", categoria=" + categoria + "]";
+	}
+	
+	
 }
